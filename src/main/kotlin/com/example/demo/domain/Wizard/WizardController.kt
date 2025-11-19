ec9338d07c3d7fe915665053651666a4e088c008
sehyun <o.sehyun@icloud.com>
2025-11-19 15:03:03 +0900

    Wizard commit

--- src/main/kotlin/com/example/demo/domain/Wizard/WizardController.kt
+++ src/main/kotlin/com/example/demo/domain/Wizard/WizardController.kt
@@ -4,15 +4,18 @@ import com.example.demo.domain.wizard.dto.WizardRecommendRequest
 import com.example.demo.domain.wizard.service.WizardService
 import org.springframework.web.bind.annotation.*
 
+// Wizard 관련 API 컨트롤러
 @RestController
 @RequestMapping("/api/wizard")
 class WizardController(
     private val wizardService: WizardService
 ) {
 
+    // 질문과 답변 리스트 제공
     @GetMapping("/questions")
     fun getQuestions() = wizardService.getWizardQuestions()
 
+    // 추천 실행 API
     @PostMapping("/recommend")
     fun recommend(
         @RequestBody request: WizardRecommendRequest,
